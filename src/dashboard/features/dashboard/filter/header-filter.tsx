eec7425adaba1f5f8b093d3caf47f676c579b88a
thivy <thivyakanth@gmail.com>
2024-07-08 18:15:09 +1000

    refactor: Update variant of reset button in header filter component

--- src/dashboard/features/dashboard/filter/header-filter.tsx
+++ src/dashboard/features/dashboard/filter/header-filter.tsx
@@ -35,7 +35,7 @@ export function Filters() {
         selectedItems={selectedEditors}
       />
       <Button
-        variant={"default"}
+        variant={"secondary"}
         size={"icon"}
         onClick={() => resetAllFilters()}
       >

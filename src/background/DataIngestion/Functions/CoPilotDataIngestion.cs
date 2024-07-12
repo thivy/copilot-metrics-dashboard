be6ac16c0f8336c669bcc0c40064bc334b9674c8
thivy <thivyakanth@gmail.com>
2024-07-12 16:19:44 +1000

    add REST api to azure function

--- src/background/DataIngestion/Functions/CoPilotDataIngestion.cs
+++ src/background/DataIngestion/Functions/CoPilotDataIngestion.cs
@@ -1,41 +1,28 @@
-﻿using GitHub;
-using GitHub.Models;
-using GitHub.Octokit.Authentication;
-using GitHub.Octokit.Client;
-using Microsoft.Azure.Functions.Worker;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.DevOpsDashboard.DataIngestion.Domain;
 using Microsoft.Extensions.Logging;
-using Microsoft.Kiota.Abstractions.Serialization;
-using Microsoft.Kiota.Serialization.Json;
 
 namespace Microsoft.DevOpsDashboard.DataIngestion.Functions;
 
 public class CoPilotDataIngestion
 {
     private readonly ILogger _logger;
+    private readonly GitHubCopilotUsageClient usageClient;
 
-    public CoPilotDataIngestion(ILoggerFactory loggerFactory)
+    public CoPilotDataIngestion(ILoggerFactory loggerFactory, GitHubCopilotUsageClient usageClient)
     {
         _logger = loggerFactory.CreateLogger<CoPilotDataIngestion>();
+        this.usageClient = usageClient;
     }
 
-    [Function("CoPilotDataIngestion")]
-    [CosmosDBOutput(databaseName: "platform-engineering", containerName: "history",
-        Connection = "AZURE_COSMOSDB_CONNECTION_STRING", CreateIfNotExists = false)]
-    public async Task<List<Domain.CopilotUsageMetrics>> Run(
+    [Function("GitHubCopilotDataIngestion")]
+    [CosmosDBOutput(databaseName: "platform-engineering", containerName: "history", Connection = "AZURE_COSMOSDB_CONNECTION_STRING", CreateIfNotExists = true)]
+    public async Task<List<CopilotUsage>> Run(
     //[TimerTrigger("*/5 * * * * *")] TimerInfo myTimer)
     [TimerTrigger("0 0 1 * * *")] TimerInfo myTimer)
     {
-        _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
-        List<Domain.CopilotUsageMetrics> usage;
-        if (Environment.GetEnvironmentVariable("USE_TEST_DATA") == "true")
-        {
-            usage = await FetchGithubCoPilotUsageDataTest();
-        }
-        else
-        {
-            usage = await FetchGithubCoPilotUsageData();
-        }
+        _logger.LogInformation($"GitHubCopilotDataIngestion timer trigger function executed at: {DateTime.Now}");
+        List<CopilotUsage> usage = await usageClient.GetCopilotMetricsForOrgsAsync();
 
         if (myTimer.ScheduleStatus is not null)
         {
@@ -45,45 +32,4 @@ public class CoPilotDataIngestion
         return usage;
     }
 
-    private Task<List<Domain.CopilotUsageMetrics>> FetchGithubCoPilotUsageDataTest()
-    {
-        ParseNodeFactoryRegistry.DefaultInstance.ContentTypeAssociatedFactories.AddOrUpdate("application/json",
-            new JsonParseNodeFactory(), (_, _) => new JsonParseNodeFactory());
-
-        var data = typeof(CoPilotDataIngestion).Assembly.GetManifestResourceStream(
-            "Microsoft.DevOpsDashboard.DataIngestion.TestData.TestData.json")!;
-
-        return Task.FromResult(KiotaJsonSerializer.DeserializeCollection<CopilotUsageMetrics>(data)
-            .Select(Domain.CopilotUsageMetrics.FromOctokit).ToList());
-    }
-
-    private async Task<List<Domain.CopilotUsageMetrics>> FetchGithubCoPilotUsageData()
-    {
-        var githubClient = new GitHubClient(RequestAdapter.Create(
-            new TokenAuthenticationProvider("AzureCoPilotUsageIngestion",
-                Environment.GetEnvironmentVariable("GITHUB_TOKEN")!)));
-
-
-        var githubType = Environment.GetEnvironmentVariable("GITHUB_TYPE");
-
-        if (githubType == "organisation")
-        {
-            _logger.LogInformation("Fetching CoPilot data for Github Org for {Today}",
-                DateTimeOffset.UtcNow.Date.ToString("R"));
-
-            var usage = await githubClient.Orgs[Environment.GetEnvironmentVariable("GITHUB_NAME")].Copilot.Usage
-                .GetAsync();
-
-            return usage!.Select(Domain.CopilotUsageMetrics.FromOctokit).ToList();
-        }
-        else
-        {
-            _logger.LogInformation("Fetching CoPilot data for Github Enterprise {Today}",
-                DateTimeOffset.UtcNow.Date.ToString("R"));
-
-            var usage = await githubClient.Enterprises["GITHUB_NAME"].Copilot.Usage.GetAsync();
-
-            return usage!.Select(Domain.CopilotUsageMetrics.FromOctokit).ToList();
-        }
-    }
 }
